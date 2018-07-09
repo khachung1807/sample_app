c4ee118e9c591a7dceec5927b70a1e8713904518
Khac Hung <khachung1807@gmail.com>
2018-07-09 22:48:53 +0700

    Chapter 7: Sign Up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title = ""
-    base_title = "Ruby on Rails Tutorial App"
+    base_title = t("rails_app")
     base_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
